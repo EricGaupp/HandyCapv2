7174540ad6c23bc2c807737f31ea905773912f24
Eric Gaupp <eric.gaupp@gmail.com>
2019-03-10 01:50:07 -0500

    Loading state displayed in react-select component instead of using a spinner as a placeholder

--- client/src/components/DashboardAddScore.js
+++ client/src/components/DashboardAddScore.js
@@ -227,8 +227,6 @@ class DashboardAddScore extends React.Component {
 
 	render() {
 		if (this.state.redirect) return <Redirect to="/dashboard/scores" />;
-		//TODO check redux courses isFetching component and display a spinner if true
-		//if (this.props.isFetching) return <h1>Fetching...</h1>;
 
 		return (
 			<React.Fragment>
